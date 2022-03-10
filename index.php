7bd7480bd344c7f0c100f4320d31b7715b0c13bf
nurmatoff <96178121+nurmatoff@users.noreply.github.com>
2022-03-10 20:08:33 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,258 @@
+<!DOCTYPE html>
+<!-- Created By CodingNepal , ha tikka uxlatdim. Chunki tekin ekan -->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shaxsiy portfio website</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"/>
+
+</head>
+<body>
+    <div class="scroll-up-btn">
+        <i class="fas fa-angle-up"></i>
+    </div>
+    <nav class="navbar">
+        <div class="max-width">
+            <div class="logo"><a href="#">Nurmat<span>off</span></a></div>
+            <ul class="menu">
+                <li><a href="#home" class="menu-btn">Bosh sahifa</a></li>
+                <li><a href="#about" class="menu-btn">Haqida</a></li>
+                <li><a href="#services" class="menu-btn">Hizmatlar</a></li>
+                <li><a href="#skills" class="menu-btn">Ko'nikma</a></li>
+                <li><a href="#teams" class="menu-btn">Jamoa</a></li>
+                <li><a href="#contact" class="menu-btn">Aloqa</a></li>
+            </ul>
+            <div class="menu-btn">
+                <i class="fas fa-bars"></i>
+            </div>
+        </div>
+    </nav>
+
+    <!-- home section start -->
+    <section class="home" id="home">
+        <div class="max-width">
+            <div class="home-content">
+                <div class="text-1">Salom, men</div>
+                <div class="text-2">Muhammaddiyor</div>
+                <div class="text-3">Nurmatov <span class="typing"></span></div>
+                <a href="https://youtube.com/channel/UC2CSphEsWMcFwR4PBqa9jPw">YouTube 2.0</a>
+            </div>
+        </div>
+    </section>
+
+    <!-- about section start -->
+    <section class="about" id="about">
+        <div class="max-width">
+            <h2 class="title">Men haqimda</h2>
+            <div class="about-content">
+                <div class="column left">
+                    <img src="images/profile-1.jpeg" alt="Bir oz kuting!">
+                </div>
+                <div class="column right">
+                    <div class="text">Men Muhammaddiyor <span class="typing-2"></span> bo'laman</div>
+                    <p>Hozirda maktab o'quvchisiman, hozirgi kunda dasturlashda o'qib kelaman. Menimcha "Junior" darajadaman. Undan tashqari SMM va PhotoDizaynerlik bilan faoliyat ham yuritib kelaman. Hali buyuk g'oyalarni amalga oshirishga intilmoqdaman. Batafsil bilish uchun pastdagi tugmani bosing! </p>
+                    <a href="#">Tarjimai hol</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- services section start -->
+    <section class="services" id="services">
+        <div class="max-width">
+            <h2 class="title">Mening hizmatlarim</h2>
+            <div class="serv-content">
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-paint-brush"></i>
+                        <div class="text">Photoshop</div>
+                        <p>Rasmlarga ishlov berish, shuningdek idealarga asoslangan barcha turdagi reklama bannerlar, shablonlar tayyorlash uchun mukammal jamoa hizmati </p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-chart-line"></i>
+                        <div class="text">Reklama</div>
+                        <p>Turli hildagi ijtimoiy tarmoqlarda reklama joylashga hissa qo'shaman va shu qatorda o'zimning ham reklama paketlarini taqdim etaman.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-code"></i>
+                        <div class="text">Web Dizaynerlik</div>
+                        <p>Huddi shu kabi portfilio yoki sizning biznesingiz rivoji uchun sifatli web-sitelar tayyorlab berish.</p>
+                    </div>
+                </div>
+               </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- skills section start -->
+    <section class="skills" id="skills">
+        <div class="max-width">
+            <h2 class="title">Mening mahoratim</h2>
+            <div class="skills-content">
+                <div class="column left"><b></b>
+                    <div class="text">Mening ijodiy qobilyatim va tajribam</div>
+                    <p>Men web-dasturchiman.</p>
+                    <a href="#">Read more</a>
+                </div>
+                <div class="column right">
+                    <div class="bars">
+                        <div class="info">
+                            <span>HTML</span>
+                            <span>70%</span>
+                        </div>
+                        <div class="line html"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>CSS</span>
+                            <span>50%</span>
+                        </div>
+                        <div class="line css"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>JavaScript</span>
+                            <span>10%</span>
+                        </div>
+                        <div class="line js"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>PHP</span>
+                            <span>70%</span>
+                        </div>
+                        <div class="line php"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>MySQL</span>
+                            <span>50%</span>
+                        </div>
+                        <div class="line mysql"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- teams section start -->
+    <section class="teams" id="teams">
+        <div class="max-width">
+            <h2 class="title">Jamoam</h2>
+            <div class="carousel owl-carousel">
+                <div class="card">
+                    <div class="box">
+                        <img src="images/profile-1.jpeg" alt="">
+                        <div class="text">Someone name</div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="images/profile-2.jpeg" alt="">
+                        <div class="text">Someone name</div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="images/profile-3.jpeg" alt="">
+                        <div class="text">Someone name</div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="images/profile-4.jpeg" alt="">
+                        <div class="text">Someone name</div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="images/profile-5.jpeg" alt="">
+                        <div class="text">Someone name</div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- contact section start -->
+    <section class="contact" id="contact">
+        <div class="max-width">
+            <h2 class="title">Men bilan bog'lanish</h2>
+            <div class="contact-content">
+                <div class="column left">
+                    <div class="text">Men haqimda</div>
+                    <p>Assalomu alaykum! Men siz va sizning biznesingizga o'zimni va jamoamning xizmatlarini taqdim etaman. Agar yordamim kerak bo'lsa quyidagi manzillarga bog'laning.</p>
+                    <div class="icons">
+                        <div class="row">
+                            <i class="fas fa-user"></i>
+                            <div class="info">
+                                <div class="head">Men</div>
+                                <div class="sub-title">Nurmatov Muhammaddiyor</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-map-marker-alt"></i>
+                            <div class="info">
+                                <div class="head">Manzilimiz</div>
+                                <div class="sub-title">Altarik, Uzbekistan</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-envelope"></i>
+                            <div class="info">
+                                <div class="head">Email</div>
+                                <div class="sub-title">xayrullayevich2@gmail.com</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column right">
+                    <div class="text">Habar yuboring</div>
+                    <form action="#">
+                        <div class="fields">
+                            <div class="field name">
+                                <input type="text" placeholder="Ismingiz" required>
+                            </div>
+                            <div class="field email">
+                                <input type="email" placeholder="Email pochtangiz" required>
+                            </div>
+                        </div>
+                        <div class="field">
+                            <input type="text" placeholder="Kasbingiz" required>
+                        </div>
+                        <div class="field textarea">
+                            <textarea cols="30" rows="10" placeholder="Habaringiz.." required></textarea>
+                        </div>
+                        <div class="button-area">
+                            <button type="submit">Habarni yuborish</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- footer section start -->
+    <footer>
+        <span>Created By <a href="https://www.nurmat_off.com">nurmat_off</a> | <span class="far fa-copyright"></span> 2022 All rights reserved.</span>
+    </footer>
+
+    <script src="script.js"></script>
+</body>
+</html>
